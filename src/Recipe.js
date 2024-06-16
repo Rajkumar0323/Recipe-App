1e659bb2ba54758361a46bf57379673e90c11019
Rajkumar0323 <140163703+Rajkumar0323@users.noreply.github.com>
2024-06-17 02:51:12 +0530

    Update Recipe.js

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,22 +1,25 @@
-import React from "react"; 
-import style from './recipe.module.css'; 
+import React, { useState } from "react";
+import style from "./recipe.module.css";
+import { FiHeart } from "react-icons/fi";
 
-const Recipe = ({title,calories,image,ingredients}) =>{ 
-	return( 
-		<div className={style.recipe}> 
-			<h1>{title}</h1> 
-			<ol> 
-				{ingredients.map(ingredient=>( 
-					<li>{ingredient.text}</li> 
-				))} 
-			</ol> 
-			
-<p>Calories : {calories}</p> 
+const Recipe = ({ title, calories, image, ingredients, isFavorited, onToggleFavorite }) => {
+  return (
+    <div className={style.recipe}>
+      <div className={style.header}>
+        <h1>{title}</h1>
+        <button onClick={onToggleFavorite} className={style.favorite}>
+          <FiHeart className={`${style.heart} ${isFavorited ? style.favorited : ''}`} />
+        </button>
+      </div>
+      <ol>
+        {ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient.text}</li>
+        ))}
+      </ol>
+      <p>Calories: {calories}</p>
+      <img className={style.image} src={image} alt="" />
+    </div>
+  );
+};
 
-			<img className={style.image} src={image} alt=""/> 
-
-		</div> 
-	); 
-
-} 
 export default Recipe;
